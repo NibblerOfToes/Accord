9e4533f20fe5408f516d06a1ab5b1b8ffb56bf6a
NibblerOfToes <scarymannibblesyourtoes@gmail.com>
2025-10-15 16:19:22 +1100

    THE FINAL FINAL COMMIT

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,43 +1,59 @@
 const assets = [
   "/",
-  "static/css/style.css",
-  "static/js/app.js",
-  "static/images/logo.png",
-  "static/images/favicon.png",
-  "static/icons/icon-128x128.png",
-  "static/icons/icon-192x192.png",
-  "static/icons/icon-384x384.png",
-  "static/icons/icon-512x512.png",
-  "static/icons/desktop_screenshot.png",
-  "static/icons/mobile_screenshot.png",
+  "/static/css/style.css",
+  "/static/css/base.css",
+  "/static/css/chat.css",
+  "/static/css/home.css",
+  "/static/css/layout.css",
+  "/static/css/login.css",
+  "/static/css/schedule.css",
+  "/static/css/signup.css",
+  "/static/css/speech.css",
+  "/static/js/app.js",
+  "/static/js/chat.js",
+  "/static/js/signup.js",
+  "/static/images/logo.png",
+  "/static/images/Accord.png",
+  "/static/images/Chat.png",
+  "/static/images/home.png",
+  "/static/images/Homepage.png",
+  "/static/images/jilm.png",
+  "/static/images/Login.png",
+  "/static/images/messages.png",
+  "/static/images/newalgorithmdesign.png",
+  "/static/images/sendmessage.png",
+  "/static/images/Signup.png",
+  "/static/images/timetable.png",
+  "/static/images/timetableicon.png",
+  "/static/images/Timetablepage.png",
+  "/static/images/user.png",
+  "/static/images/week3alternate.png",
 ];
 
 const CATALOGUE_ASSETS = "catalogue-assets";
 
-self.addEventListener("install", (installEvt) => {
-  installEvt.waitUntil(
-    caches
-      .open(CATALOGUE_ASSETS)
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CATALOGUE_ASSETS)
       .then((cache) => {
-        console.log(cache);
-        cache.addAll(assets);
+        console.log("Caching assets...");
+        return cache.addAll(assets);
       })
-      .then(self.skipWaiting())
+      .then(() => self.skipWaiting())
       .catch((e) => {
-        console.log(e);
+        console.error("Install error:", e);
       })
   );
 });
 
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches
-      .keys()
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
       .then((keyList) => {
         return Promise.all(
           keyList.map((key) => {
-            if (key === CATALOGUE_ASSETS) {
-              console.log("Removed old cache from", key);
+            if (key !== CATALOGUE_ASSETS) {
+              console.log("Removing old cache:", key);
               return caches.delete(key);
             }
           })
@@ -47,11 +63,11 @@ self.addEventListener("activate", function (evt) {
   );
 });
 
-self.addEventListener("fetch", function (evt) {
-  evt.respondWith(
-    fetch(evt.request).catch(() => {
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
       return caches.open(CATALOGUE_ASSETS).then((cache) => {
-        return cache.match(evt.request);
+        return cache.match(event.request);
       });
     })
   );
